743e76384f58dc489e52ec45fd6d4b937940b312
AirBusApplications <37170618+AirBusApplications@users.noreply.github.com>
2018-03-19 21:18:51 +0530

    Delete start.sh
    Delete start.sh

--- startup.sh
+++
@@ -1,3 +0,0 @@
-aws s3 cp s3://codepipeline-us-east-1-182860768180/CodePipelineMarch12/MyAppBuild/2mV5kzp /tmp
-unzip /tmp/2mV5kzp
-mv /tmp/target/simple-web-app.war /opt/bitnami/apache-tomcat/webapps
